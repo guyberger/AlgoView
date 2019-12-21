2bdbe0f1d6d8c303f04a652195118d18ab337ddf
guyberger <guyber1992@gmail.com>
2019-12-21 22:35:21 +0200

    finished search algorithms

--- js/search.js
+++ js/search.js
@@ -7,13 +7,83 @@ var h = grid - 1;
 var starting_point = [grid * 10 + 1, grid * 15 + 1];
 var end_point = [grid * 40 + 1, grid * 15 + 1];
 
+cleanCanvas(id);
+
+const _top = 0;
+const parent = i => ((i + 1) >>> 1) - 1;
+const left = i => (i << 1) + 1;
+const right = i => (i + 1) << 1;
+
+class PriorityQueue {
+  constructor(comparator = (a, b) => a > b) {
+    this._heap = [];
+    this._comparator = comparator;
+  }
+  size() {
+    return this._heap.length;
+  }
+  isEmpty() {
+    return this.size() == 0;
+  }
+  peek() {
+    return this._heap[_top];
+  }
+  push(...values) {
+    values.forEach(value => {
+      this._heap.push(value);
+      this._siftUp();
+    });
+    return this.size();
+  }
+  pop() {
+    const poppedValue = this.peek();
+    const bottom = this.size() - 1;
+    if (bottom > _top) {
+      this._swap(_top, bottom);
+    }
+    this._heap.pop();
+    this._siftDown();
+    return poppedValue;
+  }
+  replace(value) {
+    const replacedValue = this.peek();
+    this._heap[_top] = value;
+    this._siftDown();
+    return replacedValue;
+  }
+  _greater(i, j) {
+    return this._comparator(this._heap[i], this._heap[j]);
+  }
+  _swap(i, j) {
+    [this._heap[i], this._heap[j]] = [this._heap[j], this._heap[i]];
+  }
+  _siftUp() {
+    let node = this.size() - 1;
+    while (node > _top && this._greater(node, parent(node))) {
+      this._swap(node, parent(node));
+      node = parent(node);
+    }
+  }
+  _siftDown() {
+    let node = _top;
+    while (
+      (left(node) < this.size() && this._greater(left(node), node)) ||
+      (right(node) < this.size() && this._greater(right(node), node))
+    ) {
+      let maxChild = (right(node) < this.size() && this._greater(right(node), left(node))) ? right(node) : left(node);
+      this._swap(node, maxChild);
+      node = maxChild;
+    }
+  }
+}
+
 function resizeCanvas(id){
     var canvas = document.getElementById(id);
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 }
 
-var drawGrid = function(w, h, id) {
+ function drawGrid(w, h, id) {
     var canvas = document.getElementById(id);
     var ctx = canvas.getContext('2d');
     ctx.canvas.width  = w;
@@ -61,15 +131,11 @@ function drawStartingPosition(id, grid){
 
 }
 
-resizeCanvas(id);
-drawGrid(window.innerWidth, window.innerHeight, id_graph);
-drawStartingPosition(id, grid);  // width of box on grid
-
 function cleanCanvas(id){
     var canvas = document.getElementById(id);
     var ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawGrid(window.innerWidth, window.innerHeight, id_graph);
+    drawGrid(window.innerWidth, window.innerHeight, id);
     drawStartingPosition(id, grid);  // width of box on grid
 }
 
@@ -93,8 +159,17 @@ function drawBfsStage(id, q, len){
             break;
         }
         var p = path[path.length - 1];
-        drawRectangle(id, p[0], p[1], w, h, 'green');
+        if(p[0] != end_point[0] || p[1] != end_point[1]){
+            drawRectangle(id, p[0], p[1], w, h, 'green');
+        }  
     }
+}
+
+// heuristic is the cartesian distance
+function heuristicAstar(curr_point){
+    var x = curr_point[0], y = curr_point[1];
+    var ex = end_point[0], ey = end_point[1];
+    return Math.pow(Math.pow(y - ey, 2) + Math.pow(x - ex, 2), 0.5);
 }
 
 // paths are nested arrays
@@ -113,6 +188,22 @@ async function bfs(){
         var px = p[0];
         var py = p[1];
 
+        // reached goal
+        if(px == end_point[0] && py == end_point[1]){
+            for(var i = 0; i < curr_path.length; i++){
+                var point = curr_path[i];
+                var x = point[0];
+                var y = point[1];
+                // Skip start and end drawing
+                if((x == starting_point[0] && y == starting_point[1]) || (x == end_point[0] && y == end_point[1])){
+                    continue;
+                }
+                drawRectangle(id, x, y, grid - 1, grid - 1, 'yellow');
+                await sleep(time_sleep_fast);
+            }
+            break;
+        }
+
         // draw stage
         if(curr_path.length - 1 >= dist){
             console.log("stage: " + dist);
@@ -120,21 +211,6 @@ async function bfs(){
             drawBfsStage(id, q, curr_path.length);
             drawRectangle(id, px, py, w, h, 'green');
             await sleep(time_sleep);
-        }
-
-
-        // reached goal
-        if(px == end_point[0] && py == end_point[1]){
-            for(var i = 0; i < curr_path.length; i++){
-                var point = curr_path[i];
-                // Skip start and end drawing
-                if((x == starting_point[0] && y == starting_point[1]) || (x == end_point[0] && y == end_point[1])){
-                    continue;
-                }
-                drawRectangle(id, point[0], point[1], grid - 1, grid - 1, 'yellow');
-                await sleep(time_sleep_fast);
-            }
-            break;
         }
 
         // Adding neighbours
@@ -161,5 +237,139 @@ async function bfs(){
                 q.push(new_path);
             }
         }
+    }
+}
+
+async function dfs(){
+    cleanCanvas(id);
+    var stack = [];
+    var starting_path = [{}, starting_point];   // first item is the path history tiles
+    stack.push(starting_path);
+
+    // perform dfs search
+    while(stack.length > 0){
+        var curr_path = stack.pop();  // get oldest path in queue.    path: [[x1, y1], [x2, y2], ...]
+        var p = curr_path[curr_path.length - 1];    // get latest point in path.    p: [x, y]
+        var px = p[0];
+        var py = p[1];
+
+        // reached goal
+        if(px == end_point[0] && py == end_point[1]){
+            for(var i = 1; i < curr_path.length; i++){
+                var point = curr_path[i];
+                var x = point[0];
+                var y = point[1];
+                // Skip start and end drawing
+                if((x == starting_point[0] && y == starting_point[1]) || (x == end_point[0] && y == end_point[1])){
+                    continue;
+                }
+                drawRectangle(id, x, y, grid - 1, grid - 1, 'yellow');
+                await sleep(time_sleep_fast);
+            }
+            break;
+        }
+
+        // Adding neighbours
+        for(var dx = -1; dx < 2; dx++){
+            for(var dy = -1; dy < 2; dy++){
+                if(dy * dy == dx * dx){     // skip diagonal neighbours
+                    continue;
+                }
+                var x = px + (dx * grid);
+                var y = py + (dy * grid);
+                var s = x.toString() + "+" + y.toString();
+                if(s in curr_path[0] || x < 0 || y < 0 || x >= window.width || y >= window.height){
+                    continue;
+                }
+                curr_path[0][s] = 1;
+                
+                // Skip start and end drawing
+                if((x == starting_point[0] && y == starting_point[1])){
+                    continue;
+                }
+                if(x != end_point[0] || y != end_point[1]){
+                    drawRectangle(id, x, y, grid - 1, grid - 1, 'green');
+                }
+                let new_path = JSON.parse(JSON.stringify(curr_path));   // deep clone
+                new_path.push([x, y]);     // add neighbour to current path
+                stack.push(new_path);
+            }
+        }
+        await sleep(time_sleep);
+    }
+}
+
+async function dijkstra(){
+    bfs();  // for unweighted graph
+}
+
+async function astar(){
+    cleanCanvas(id);
+    var pq = new PriorityQueue((a, b) => a[0] < b[0]);
+    var dist_to_goal = heuristicAstar(starting_point);
+    var starting_path = [starting_point];   // first item is the 
+    pq.push([dist_to_goal, starting_path]);
+
+    // perform dfs search
+    while(!pq.isEmpty()){
+        var curr_path_with_hu = pq.pop();   // get best priority path (shortest dist).    path: [[x1, y1], [x2, y2], ...]
+        var curr_path = curr_path_with_hu[1];
+        var curr_hu = curr_path_with_hu[0];  
+        var curr_dist = curr_path.length - 1;   // not including first node
+        var to_beat = curr_dist + curr_hu;  // best distance + huristic path to goal
+        var p = curr_path[curr_path.length - 1];    // get latest point in path.    p: [x, y]
+        var px = p[0];
+        var py = p[1];
+
+        // reached goal
+        if(px == end_point[0] && py == end_point[1]){
+            for(var i = 1; i < curr_path.length; i++){
+                var point = curr_path[i];
+                var x = point[0];
+                var y = point[1];
+                // Skip start and end drawing
+                if((x == starting_point[0] && y == starting_point[1]) || (x == end_point[0] && y == end_point[1])){
+                    continue;
+                }
+                drawRectangle(id, x, y, grid - 1, grid - 1, 'yellow');
+                await sleep(time_sleep_fast);
+            }
+            break;
+        }
+
+        // Adding neighbours
+        for(var dx = -1; dx < 2; dx++){
+            for(var dy = -1; dy < 2; dy++){
+                if(dy * dy == dx * dx){     // skip diagonal neighbours
+                    continue;
+                }
+                var x = px + (dx * grid);
+                var y = py + (dy * grid);
+                if(x < 0 || y < 0 || x >= window.width || y >= window.height){
+                    continue;
+                }
+                
+                // Skip start and end drawing
+                if((x == starting_point[0] && y == starting_point[1])){
+                    continue;
+                }
+
+                // filter neighbours with worse astar dist
+                var n_huristic = heuristicAstar([x, y]);
+                var n_dist = curr_dist + 1;
+                var n_astar_dist = n_huristic + n_dist;
+                if(n_astar_dist > to_beat){
+                    continue;
+                }
+
+                if(x != end_point[0] || y != end_point[1]){
+                    drawRectangle(id, x, y, grid - 1, grid - 1, 'green');
+                }
+                let new_path = JSON.parse(JSON.stringify(curr_path));   // deep clone
+                new_path.push([x, y]);     // add neighbour to current path
+                pq.push([n_astar_dist, new_path]);
+            }
+        }
+        await sleep(time_sleep);
     }
 }
